c82cf62cff46c645bc4c7d328773ada9490ad5f2
DimasWahyuH <dimaswh19@gmail.com>
2024-05-10 15:56:46 +0700

    Menambahkan modifier publik dalam node

--- stack using linked list/stack using linked list.cpp
+++ stack using linked list/stack using linked list.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+//NOde class reprenting a single node in the linked list
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node() {
+        next = NULL;
+    }
+};
